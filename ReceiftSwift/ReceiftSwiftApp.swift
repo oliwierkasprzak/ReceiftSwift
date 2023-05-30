45cfed8d0aec5d52e2c990029a0bfe044262e99f
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-05-30 19:41:13 +0200

    Initial Commit

---
+++ ReceiftSwift/ReceiftSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiftSwiftApp.swift
+//  ReceiftSwift
+//
+//  Created by Oliwier Kasprzak on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReceiftSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
